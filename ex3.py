f4c68ba04316d0bcc48b5d974dc39269c25c1c74
nikefido <fideloper@gmail.com>
2012-04-24 12:08:12 -0400

    Lessons 1-5

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+#These originally were integers. Adding .0 made the Floats, for more true math
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
