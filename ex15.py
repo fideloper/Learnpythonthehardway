782911f8c59d06ae8861df983d36ae89a0c54a96
fideloper <fideloper@gmail.com>
2012-04-27 21:22:54 -0400

    Up to 15

---
+++ ex15.py
@@ -0,0 +1,23 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
+
+txt.close();
+
+# command line: 
+#python
+#>>> txt = open('ex15_example.txt')
+#>>> txt.read()
+#>>> txt.close()
